ee951a8ff68cefabcc769c12663dc01ffd547a9e
Dardo Altobelli <altobellidardo@gmail.com>
2026-02-27 16:35:08 -0300

    feaat: use window.crypto if its avaible

--- src/lib/uuid.ts
+++ src/lib/uuid.ts
@@ -1,3 +1,6 @@
 export function randomUUID () {
+  if (typeof crypto !== 'undefined' && crypto.randomUUID) {
+    return crypto.randomUUID()
+  }
   return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
 }
