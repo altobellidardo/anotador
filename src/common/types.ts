ea75e2d586c6b48c99fc8d62e352a7ce92048f55
Dardo Altobelli <altobellidardo@gmail.com>
2026-02-26 22:32:29 -0300

    feat: update basics types

--- src/common/types.ts
+++ src/common/types.ts
@@ -1,14 +1,27 @@
+import type { StoreApi, UseBoundStore } from 'zustand'
+
 export type TrucoTeam = 'nosotros' | 'ellos'
 export type TrucoScores = Record<TrucoTeam, number>
 
-export interface BerePlayer {
+export interface BasePlayer {
   id: string
   name: string
+}
+
+export interface BerePlayer extends BasePlayer {
   scores: number[]
 }
 
-export interface ChanPlayer {
-  id: string
-  name: string
+export interface ChanPlayer extends BasePlayer {
   score: number
 }
+
+export type PedroPlayer = ChanPlayer
+
+export interface PlayerStoreState<TPlayer extends BasePlayer> {
+  players: TPlayer[]
+  resetGame: () => void
+  addPlayer: (player: TPlayer) => void
+}
+
+export type PlayerStoreHook<TPlayer extends BasePlayer> = UseBoundStore<StoreApi<PlayerStoreState<TPlayer>>>
